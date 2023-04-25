61d81b0ec8fc4a0cffb98c99c6b69d44866e1353
Heshaam Attar <heshaam.attar@42crunch.com>
2023-04-25 00:08:13 -0700

    Update OAS

--- fetchOAS.py
+++ fetchOAS.py
@@ -1,1 +1,9 @@
+import requests
+
+
 print('Hello World')
+
+url = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v2.0/json/petstore-simple.json'
+r = requests.get(url)
+
+print(r.json())
